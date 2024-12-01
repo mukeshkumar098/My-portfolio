580ed13b5a6802050e25c2da8dc7ca81b8d13480
mukeshkumar098 <mukeshkemar567@gmail.com>
2024-12-01 22:23:56 +0530

    check

--- src/Pages/Page1.jsx
+++ src/Pages/Page1.jsx
@@ -55,7 +55,7 @@ const Page1 = () => {
                 mouseMoving(e)
             }} className='h-screen px-2 py-1 bg-white flex items-center justify-center'>
  
-                <div id='tilt-in' className=" relative h-full bg-transparent w-full rounded-[20px] !bg-[url('logo.jpg')] bg-cover bg-left bg-no-repeat ">
+                <div id='tilt-in' className=" relative h-full bg-transparent w-full rounded-[20px] bg-[url('logo.jpg')] bg-cover bg-left bg-no-repeat ">
                   <Header/>
                     <div className='flex justify-between flex-wrap gap-24 mt-16  items-center  '>
                         <div className='pl-10 '>
