9244b8b1db908d4f93b1f7ee563d5fb35f018a07
mukeshkumar098 <mukeshkemar567@gmail.com>
2024-12-06 11:34:43 +0530

    check

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -16,7 +16,7 @@ const Header = ({onButtonClick}) => {
  
   return (
     <>
-      <div    className='w-full h-auto  bg-white flex  justify-center '>
+      <div    className='w-full sticky top-0 left-0 right-0 z-30 h-auto  bg-white flex  justify-center '>
 
         <div className='flex w-full px-1  justify-between items-center bg-black'>
           <img className='w-16 ml-10' src="/logos.png" alt="logo" />
@@ -33,9 +33,9 @@ const Header = ({onButtonClick}) => {
             <div className=''>
             <FaBars onClick={()=>setBar(!bar)} size={40} className='block text-white p-2 z-10   sm:hidden ' />
             {bar && <div className='sm:hidden absolute  text-white right-4  top-20'>
-              <div className='w-52 px-10 py-2 bg-black shadow-lg shadow-cyan-200 -950 rounded-lg'>
-               <div onClick={()=>setBar(!bar)} className='text-white  flex justify-end'><ImCross size={12}/></div>
-                <ul className='flex flex-col gap-8 pb-10 '>
+              <div className='w-52  bg-black shadow-lg shadow-cyan-200 -950 rounded-lg'>
+               <div onClick={()=>setBar(!bar)} className='text-white  flex px-5 py-1 justify-end'><ImCross size={12}/></div>
+                <ul className='flex  py-2 px-10 flex-col gap-8 pb-10 '>
                   <li className='flex items-center text-xl gap-2'><IoPerson size={20}/><p>About</p></li>
                   <li className='flex items-center text-xl gap-2'><FaPhoneAlt size={20}/><p>Contect</p></li>
                   <li className='flex items-center text-xl gap-2'><AiFillProject size={20}/><p>Project</p></li>
